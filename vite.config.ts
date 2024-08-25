ce0338ed3258f2941e86ebb230f5ccb27ef1ce2d
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-08-25 21:35:44 +0000

    Add cover art

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [
     remix({
       ssr: false,
+      basename: '/cover-art',
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
