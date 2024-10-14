91cd93e804f0626c213ee5f8a96e7a11b3bdd41c
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-10-14 20:16:51 +0000

    Replace dynamic

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,12 +1,13 @@
+import { render } from '../build/server/index.js'
+
 const readManifest = async () => {
   const data = await import('../build/build-manifest.json')
   return data
 }
 
 const serverRender = async (c, assetMap) => {
-  const importedApp = await import('../build/server/index.js')
   const ua = c.request.headers.get('user-agent')
-  const response = await importedApp.render({
+  const response = await render({
     request: c.request,
     ua,
     assetMap
@@ -20,14 +21,15 @@ export const onRequest = async (c) => {
     const res = await serverRender(c, manifest)
     return res
   } catch (err) {
-    console.error('SSR render error\n', err)
     if (err instanceof Response) {
-        return err
-    }
-    return new Response('<h1>Something went wrong</h1>', {
+      return err
+    } else {
+      console.error('SSR render error\n', err)
+      return new Response('<h1>Something went wrong</h1>', {
         status: 500,
         headers
       })
+    }
   }
 }
 
