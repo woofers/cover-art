b42f5832d0bf390a4436702cdf534864dee8c6da
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-10-14 20:09:07 +0000

    Remove suspense routes

--- app/routes.tsx
+++ app/routes.tsx
@@ -37,10 +37,6 @@ export const routes = [
         }
       },
       {
-        path: '/suspense',
-        Component: React.lazy(() => import('./routes/suspense'))
-      },
-      {
         path: '/redirect',
         loader: async () => {
           return redirect('/about')
