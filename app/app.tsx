d92ec5ae9d6b317818844e6eb4b4e8a5c92f5343
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-10-19 18:59:59 +0000

    Add template start

--- app/app.tsx
+++ app/app.tsx
@@ -11,6 +11,13 @@ const App: React.FC<{ assetMap: AssetMap; children: React.ReactNode }> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet"
+      />
+      <title>React + Rsbuild + SSR</title>
       {assetMap.css?.map(style => (
         <link key={style} rel="stylesheet" href={style} />
       ))}
